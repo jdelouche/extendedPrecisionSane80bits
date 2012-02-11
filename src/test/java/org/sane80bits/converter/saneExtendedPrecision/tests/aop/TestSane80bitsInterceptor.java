7d3c5f40b56dc8576fa46056bf5b55b75c53e60d
Jean-Pierre Delouche <jdelouche@gmail.com>
2012-02-11 12:32:18 +0100

    aop into the testng reporter

--- src/test/java/org/sane80bits/converter/saneExtendedPrecision/tests/aop/TestSane80bitsInterceptor.java
+++ src/test/java/org/sane80bits/converter/saneExtendedPrecision/tests/aop/TestSane80bitsInterceptor.java
@@ -2,18 +2,23 @@ package org.sane80bits.converter.saneExtendedPrecision.tests.aop;
 
 import org.jboss.aop.joinpoint.Invocation;
 import org.jboss.aop.joinpoint.MethodInvocation;
+import org.testng.Reporter;
 
 public class TestSane80bitsInterceptor implements org.jboss.aop.advice.Interceptor {
 	public Object invoke(Invocation invocation) throws Throwable {
 		java.lang.reflect.Method m = ((MethodInvocation) invocation).getMethod();
-		System.out.println("=> " + m.getName());
+		String msg = "=> " + m.getName();
+		System.out.println(msg);
+		Reporter.log(msg);
 		long startTime = System.currentTimeMillis();
 		try {
 			return invocation.invokeNext();
 		} finally {
 			long endTime = System.currentTimeMillis() - startTime;
-			System.out.println("=< " + m.getName() + " time: " + endTime
-					+ "ms");
+			msg = "=< " + m.getName() + " time: " + endTime
+					+ "ms";
+			System.out.println(msg);
+			Reporter.log(msg);
 		}
 	}
 
